02d780468c5861ae35b8b7d132208ebcf102af8b
Aaron Lun <infinite.monkeys.with.keyboards@gmail.com>
2019-01-03 01:54:30 +0000

    Subset directly rather than via subset() to keep CHECK happy. Other fixes to linker for CHECK.

--- R/linker.R
+++ R/linker.R
@@ -1,11 +1,10 @@
 #' @importFrom ExperimentHub ExperimentHub
-#' @importMethodsFrom AnnotationHub subset
 .linker <- function(dataset, sra, outdir=tempfile(), hub=ExperimentHub())
 # Creates links from the BAM files and indices.
 {
     targets <- file.path("chipseqDBData", dataset, sprintf("%s.bam", sra))
     if (!file.exists(outdir)) {
-        dir.create(outdir, showWarning=FALSE)
+        dir.create(outdir, showWarnings=FALSE)
     }
     output <- file.path(outdir, sprintf("%s.bam", sra))
 
@@ -14,12 +13,12 @@
         outbam <- output[i]
         outbai <- paste0(outbam, ".bai")
 
-        curbam <- subset(hub, rdatapath == curtarget)[[1]]
+        curbam <- hub[hub$rdatapath==curtarget][[1]]
         if (!file.symlink(curbam, outbam)) {
             stop(sprintf("failed to create symbolic link at '%s'", outbam))
         }
 
-        curbai <- subset(hub, rdatapath == paste0(curtarget, ".bai"))[[1]]
+        curbai <- hub[hub$rdatapath==paste0(curtarget, ".bai")][[1]]
         if (!file.symlink(curbai, outbai)) {
             stop(sprintf("failed to create symbolic link at '%s'", outbai))
         }
