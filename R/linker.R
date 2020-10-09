944a58d3ae65440019c14f723b6a7e345b5b4d36
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2020-10-08 22:19:16 -0700

    Report BamFiles to handle variable index paths.

--- R/linker.R
+++ R/linker.R
@@ -1,45 +1,16 @@
+#' @importFrom S4Vectors List
+#' @importFrom Rsamtools BamFile
 #' @importFrom ExperimentHub ExperimentHub
-.linker <- function(dataset, sra, outdir=tempfile(), hub=ExperimentHub())
-# Creates links from the BAM files and indices.
-{
+.linker <- function(dataset, sra, outdir=NA_character_, hub=ExperimentHub()) {
     targets <- file.path("chipseqDBData", dataset, sprintf("%s.bam", sra))
-    if (!file.exists(outdir)) {
-        dir.create(outdir, showWarnings=FALSE)
-    }
-    output <- file.path(outdir, sprintf("%s.bam", sra))
-    FUN <- .linkFUN()
+    output <- List()
 
     for (i in seq_along(targets)) {
         curtarget <- targets[i]
-        outbam <- output[i]
-        outbai <- paste0(outbam, ".bai")
-
         curbam <- hub[hub$rdatapath==curtarget][[1]]
-        FUN(curbam, outbam)
-
         curbai <- hub[hub$rdatapath==paste0(curtarget, ".bai")][[1]]
-        FUN(curbai, outbai)
+        output[[i]] <- BamFile(curbam, index=curbai)
     }
 
     output
-}
-
-.linkFUN <- function() 
-# Return a function to use hard/soft links depending on the OS.
-{
-    if (.Platform$OS.type=="windows") {
-        function(from, to) {
-            if (!file.symlink(from, to) && !file.link(from, to) && !file.copy(from, to)) {
-                stop(sprintf("failed to create link at '%s'", to))
-            }
-            invisible(NULL)
-        }
-    } else {
-        function(from, to) {
-            if (!file.symlink(from, to)) {
-                stop(sprintf("failed to create symbolic link at '%s'", to))
-            }
-            invisible(NULL)
-        }
-    }
 }
